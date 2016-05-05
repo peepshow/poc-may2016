d873ff6759ea5b9831dcb1a3c7635fe8f8685314
Rere <reganvause@gmail.com>
2016-05-06 00:37:23 +1200

    Revert "chapter base layout"
    This reverts commit bcc0fa4430ae993cf4dc181e08b984de887bf47e.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -146,211 +146,109 @@ $gallery.on( 'staticClick', function( event, pointer, cellElem, cellIndex ) {
 // });
 
 // Init controller
-// var controller = new ScrollMagic.Controller({
-//   globalSceneOptions: {
-//     duration: $('section').height(),
-//     triggerHook: .025,
-//     reverse: true
-//   }
-// });
-//
+var controller = new ScrollMagic.Controller({
+  globalSceneOptions: {
+    duration: $('section').height(),
+    triggerHook: .025,
+    reverse: true
+  }
+});
+
+
+/*
+scene_object = {
+  '[scene-name]' : {
+    '[target-scene-id]' : '[anchor-href-value]'
+  }
+}
+*/
+var scenes = {
+  'intro': {
+    'intro': 'intro-anchor'
+  },
+  'theme-1': {
+    'theme-1': 'anchor1'
+  },
+  'theme-2': {
+    'theme-2': 'anchor2'
+  },
+  'theme-3': {
+    'theme-3': 'anchor3'
+  }
+}
+
+
+
+for(var key in scenes) {
+  // skip loop if the property is from prototype
+  if (!scenes.hasOwnProperty(key)) continue;
+
+  var obj = scenes[key];
+
+  for (var prop in obj) {
+    // skip loop if the property is from prototype
+    if(!obj.hasOwnProperty(prop)) continue;
+
+    new ScrollMagic.Scene({ triggerElement: '#'+prop })
+        .setClassToggle('#'+obj[prop], 'active')
+        .addTo(controller);
+  }
+}
+
+
+// Change behaviour of controller
+// to animate scroll instead of jump
+controller.scrollTo(function(target) {
+
+  TweenMax.to(window, 0.5, {
+    scrollTo : {
+      y : target,
+      autoKill : true // Allow scroll position to change outside itself
+    },
+    ease : Cubic.easeInOut
+  });
+
+});
+
+
+// Bind scroll to anchor links using Vanilla JavaScript
+var anchor_nav = document.querySelector('.anchor-nav');
+
+anchor_nav.addEventListener('click', function(e) {
+  var target = e.target,
+      id     = target.getAttribute('href');
+
+  if(id !== null) {
+    if(id.length > 0) {
+      e.preventDefault();
+      controller.scrollTo(id);
+
+      if(window.history && window.history.pushState) {
+        history.pushState("", document.title, id);
+      }
+    }
+  }
+});
+
+
 //
 // /*
-// scene_object = {
-//   '[scene-name]' : {
-//     '[target-scene-id]' : '[anchor-href-value]'
-//   }
-// }
+//  * Bind scroll to anchor links using jQuery
 // */
-// var scenes = {
-//   'intro': {
-//     'intro': 'intro-anchor'
-//   },
-//   'theme-1': {
-//     'theme-1': 'anchor1'
-//   },
-//   'theme-2': {
-//     'theme-2': 'anchor2'
-//   },
-//   'theme-3': {
-//     'theme-3': 'anchor3'
-//   }
-// }
+// $(document).on("click", "a[href^=\\#]", function(e) {
+//   var id = $(this).attr("href");
+//   console.log "oi";
 //
+//   if($(id).length > 0) {
+//     e.preventDefault();
 //
+//     // trigger scroll
+//     controller.scrollTo(id);
 //
-// for(var key in scenes) {
-//   // skip loop if the property is from prototype
-//   if (!scenes.hasOwnProperty(key)) continue;
-//
-//   var obj = scenes[key];
-//
-//   for (var prop in obj) {
-//     // skip loop if the property is from prototype
-//     if(!obj.hasOwnProperty(prop)) continue;
-//
-//     new ScrollMagic.Scene({ triggerElement: '#'+prop })
-//         .setClassToggle('#'+obj[prop], 'active')
-//         .addTo(controller);
-//   }
-// }
-//
-//
-// // Change behaviour of controller
-// // to animate scroll instead of jump
-// controller.scrollTo(function(target) {
-//
-//   TweenMax.to(window, 0.5, {
-//     scrollTo : {
-//       y : target,
-//       autoKill : true // Allow scroll position to change outside itself
-//     },
-//     ease : Cubic.easeInOut
-//   });
-//
-// });
-//
-//
-// // Bind scroll to anchor links using Vanilla JavaScript
-// var anchor_nav = document.querySelector('.anchor-nav');
-//
-// anchor_nav.addEventListener('click', function(e) {
-//   var target = e.target,
-//       id     = target.getAttribute('href');
-//
-//   if(id !== null) {
-//     if(id.length > 0) {
-//       e.preventDefault();
-//       controller.scrollTo(id);
-//
-//       if(window.history && window.history.pushState) {
-//         history.pushState("", document.title, id);
-//       }
+//     // If supported by the browser we can also update the URL
+//     if (window.history && window.history.pushState) {
+//       history.pushState("", document.title, id);
 //     }
 //   }
+//
 // });
-
-
-(function ($) {
-
-	// Init ScrollMagic
-  var controller = new ScrollMagic.Controller();
-
-    // get all slides
-	var slides = ["#theme-1", "#theme-2", "#theme-3", "#theme-4", "#theme-5", "#theme-6", "#theme-7"];
-
-	// get all headers in slides that trigger animation
-	var headers = ["#theme-1 header", "#theme-2 header", "#theme-3 header", "#theme-4 header", "#theme-5 header", "#theme-6 header", "#theme-7 header"];
-
-	// get all break up sections
-	//var breakSections = ["#cb01", "#cb02", "#cb03"];
-
-	// Enable ScrollMagic only for desktop, disable on touch and mobile devices
-	if (!Modernizr.touch) {
-
-		// SCENE 1
-		// create scenes for each of the headers
-		headers.forEach(function (header, index) {
-
-		    // number for highlighting scenes
-			var num = index+1;
-
-		    // make scene
-		    var headerScene = new ScrollMagic.Scene({
-		        triggerElement: header, // trigger CSS animation when header is in the middle of the viewport
-		        offset: -95 // offset triggers the animation 95 earlier then middle of the viewport, adjust to your liking
-		    })
-		    .setClassToggle('#theme-'+num, 'is-active') // set class to active slide
-		    //.addIndicators() // add indicators (requires plugin), use for debugging
-		    .addTo(controller);
-		});
-
-	    // SCENE 2
-	    // change color of the nav for dark content blocks
-	  //   breakSections.forEach(function (breakSection, index) {
-    //
-		//     // number for highlighting scenes
-		// 	var breakID = $(breakSection).attr('id');
-    //
-		//     // make scene
-		//     var breakScene = new ScrollMagic.Scene({
-		//         triggerElement: breakSection, // trigger CSS animation when header is in the middle of the viewport
-		//         triggerHook: 0.75
-		//     })
-		//     .setClassToggle('#'+breakID, 'is-active') // set class to active slide
-		//     .on("enter", function (event) {
-		// 	    $('nav').attr('class','is-light');
-		// 	})
-		//     .addTo(controller);
-		// });
-
-	    // SCENE 3
-	    // change color of the nav back to dark
-		slides.forEach(function (slide, index) {
-
-			var slideScene = new ScrollMagic.Scene({
-		        triggerElement: slide // trigger CSS animation when header is in the middle of the viewport
-		    })
-		    .on("enter", function (event) {
-			    $('nav').removeAttr('class');
-			})
-		    .addTo(controller);
-	    });
-
-	    // SCENE 4 - parallax effect on each of the slides with bcg
-	    // move bcg container when slide gets into the view
-		slides.forEach(function (slide, index) {
-
-			var $bcg = $(slide).find('.bcg');
-
-			var slideParallaxScene = new ScrollMagic.Scene({
-		        triggerElement: slide,
-		        triggerHook: 1,
-		        duration: "100%"
-		    })
-		    .setTween(TweenMax.from($bcg, 1, {y: '-40%', autoAlpha: 0.3, ease:Power0.easeNone}))
-		    .addTo(controller);
-	    });
-
-	    // SCENE 5 - parallax effect on the intro slide
-	    // move bcg container when intro gets out of the the view
-
-	    var introTl = new TimelineMax();
-
-	    introTl
-	    	.to($('#intro header, .scroll-hint'), 0.2, {autoAlpha: 0, ease:Power1.easeNone})
-	    	.to($('#intro .bcg'), 1.4, {y: '20%', ease:Power1.easeOut}, '-=0.2')
-	    	.to($('#intro'), 0.7, {autoAlpha: 0.5, ease:Power1.easeNone}, '-=1.4');
-
-		var introScene = new ScrollMagic.Scene({
-	        triggerElement: '#intro',
-	        triggerHook: 0,
-	        duration: "100%"
-	    })
-	    .setTween(introTl)
-	    .addTo(controller);
-
-	    // change behaviour of controller to animate scroll instead of jump
-		controller.scrollTo(function (newpos) {
-			TweenMax.to(window, 1, {scrollTo: {y: newpos}, ease:Power1.easeInOut});
-		});
-
-		//  bind scroll to anchor links
-		$(document).on("click", "a[href^='#']", function (e) {
-			var id = $(this).attr("href");
-			if ($(id).length > 0) {
-				e.preventDefault();
-
-				// trigger scroll
-				controller.scrollTo(id);
-
-					// if supported by the browser we can even update the URL.
-				if (window.history && window.history.pushState) {
-					history.pushState("", document.title, id);
-				}
-			}
-		});
-
-	}
-
-}(jQuery));
