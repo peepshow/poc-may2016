05d4d1ef7262eef4165aa7e100c6a8f91c51dd98
Rere <reganvause@gmail.com>
2016-05-05 14:48:13 +1200

    big commit
    - scss tidy
    - videos & captions added
    - bourbon in use

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,53 +1,3 @@
-console.log('\'Allo \'Allo!');
-
-var $gallery = $('.gallery').flickity({
-  cellAlign: 'left',
-  contain: true,
-  prevNextButtons: true,
-  pageDots: false,
-});
-
-$gallery.on( 'staticClick', function( event, pointer, cellElem, cellIndex ) {
-  if ( cellIndex !== undefined ) {
-    $gallery.flickity( 'select', cellIndex );
-  }
-});
-
-
-// Config
-var boxes     = document.querySelectorAll('.js-box'),
-    box_count = boxes.length,
-    perrywax  = document.querySelector('#perrywax'),
-    box_id    = '#js-box';
-
-// Global Controller
-var controller = new ScrollMagic.Controller({
-  globalSceneOptions: {
-    triggerHook: 0,
-    reverse: true,
-        addIndicators: true
-  }
-});
-
-// Pinned Scene. Totes optional
-new ScrollMagic.Scene({
-  triggerElement: perrywax,
-  duration: '100%'
-})
-.setPin(perrywax)
-.addTo(controller);
-
-// Box Scenes
-for (var i = 1, l = box_count; i <= l; i++) {
-  new ScrollMagic.Scene({
-    triggerElement: perrywax,
-    triggerHook: 1,
-    duration: '100%'
-  })
-  .setTween(box_id + i, 0.25, { y: '-300' * i + '%', ease: Linear.easeNone })
-  .addTo(controller);
-}
-
 var $poster = $('.poster'),
   //$shine = $('.shine'),
   $layer = $('div[class*="layer-"]'),
@@ -115,4 +65,190 @@ $(window).on('mousemove click', function(e) {
 
 });
 
-moveBackground();
+//moveBackground();
+
+var $gallery = $('.gallery').flickity({
+  cellAlign: 'left',
+  contain: true,
+  prevNextButtons: true,
+  pageDots: false,
+});
+
+$gallery.on( 'staticClick', function( event, pointer, cellElem, cellIndex ) {
+  if ( cellIndex !== undefined ) {
+    $gallery.flickity( 'select', cellIndex );
+  }
+});
+
+//
+// // Config
+// var boxes     = document.querySelectorAll('.js-box'),
+//     box_count = boxes.length,
+//     perrywax  = document.querySelector('#perrywax'),
+//     box_id    = '#js-box';
+//
+// // Global Controller
+// var controller = new ScrollMagic.Controller({
+//   globalSceneOptions: {
+//     triggerHook: 0,
+//     reverse: true,
+//     addIndicators: true
+//   }
+// });
+//
+// // Pinned Scene. Totes optional
+// new ScrollMagic.Scene({
+//   triggerElement: perrywax,
+//   duration: '100%'
+// })
+// .setPin(perrywax)
+// .addTo(controller);
+//
+// // Box Scenes
+// for (var i = 1, l = box_count; i <= l; i++) {
+//   new ScrollMagic.Scene({
+//     triggerElement: perrywax,
+//     triggerHook: 1,
+//     duration: '100%'
+//   })
+//   .setTween(box_id + i, 0.25, { y: '-300' * i + '%', ease: Linear.easeNone })
+//   .addTo(controller);
+// }
+
+// $(function() {
+//
+//   $('.main_video').each(function (index, elem) {
+//     // Init ScrollMagic Controller
+//     var scrollMagicController = new ScrollMagic.Controller();
+//
+//     // Create Animations
+//     var title = $(elem).find('div'),
+//         text = $(elem).find('figcaption'),
+//         btn = $(elem).find('a');
+//
+//     var tl = new TimelineMax({pause: true});
+//     tl.add("start") // add timeline label
+//       .fromTo(title, 0.4, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+//       .fromTo(text, 0.4, { y: '60px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+//       .fromTo(btn, 0.4, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+//
+//     // Create the Scene and trigger when visible
+//     var scene = new ScrollMagic.Scene({
+//       triggerElement: elem,
+//       offset: 0
+//     })
+//     .setTween(tl)
+//     .addTo(scrollMagicController);
+//
+//     // Add debug indicators fixed on right side
+//      scene.addIndicators();
+//   });
+// });
+
+// Init controller
+var controller = new ScrollMagic.Controller({
+  globalSceneOptions: {
+    duration: $('section').height(),
+    triggerHook: .025,
+    reverse: true
+  }
+});
+
+
+/*
+scene_object = {
+  '[scene-name]' : {
+    '[target-scene-id]' : '[anchor-href-value]'
+  }
+}
+*/
+var scenes = {
+  'intro': {
+    'intro': 'intro-anchor'
+  },
+  'theme-1': {
+    'theme-1': 'anchor1'
+  },
+  'theme-2': {
+    'theme-2': 'anchor2'
+  },
+  'theme-3': {
+    'theme-3': 'anchor3'
+  }
+}
+
+
+
+for(var key in scenes) {
+  // skip loop if the property is from prototype
+  if (!scenes.hasOwnProperty(key)) continue;
+
+  var obj = scenes[key];
+
+  for (var prop in obj) {
+    // skip loop if the property is from prototype
+    if(!obj.hasOwnProperty(prop)) continue;
+
+    new ScrollMagic.Scene({ triggerElement: '#'+prop })
+        .setClassToggle('#'+obj[prop], 'active')
+        .addTo(controller);
+  }
+}
+
+
+// Change behaviour of controller
+// to animate scroll instead of jump
+controller.scrollTo(function(target) {
+
+  TweenMax.to(window, 0.5, {
+    scrollTo : {
+      y : target,
+      autoKill : true // Allow scroll position to change outside itself
+    },
+    ease : Cubic.easeInOut
+  });
+
+});
+
+
+// Bind scroll to anchor links using Vanilla JavaScript
+var anchor_nav = document.querySelector('.anchor-nav');
+
+anchor_nav.addEventListener('click', function(e) {
+  var target = e.target,
+      id     = target.getAttribute('href');
+
+  if(id !== null) {
+    if(id.length > 0) {
+      e.preventDefault();
+      controller.scrollTo(id);
+
+      if(window.history && window.history.pushState) {
+        history.pushState("", document.title, id);
+      }
+    }
+  }
+});
+
+
+//
+// /*
+//  * Bind scroll to anchor links using jQuery
+// */
+// $(document).on("click", "a[href^=\\#]", function(e) {
+//   var id = $(this).attr("href");
+//   console.log "oi";
+//
+//   if($(id).length > 0) {
+//     e.preventDefault();
+//
+//     // trigger scroll
+//     controller.scrollTo(id);
+//
+//     // If supported by the browser we can also update the URL
+//     if (window.history && window.history.pushState) {
+//       history.pushState("", document.title, id);
+//     }
+//   }
+//
+// });
